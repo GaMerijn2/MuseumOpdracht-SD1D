a9e0c7f72b7975feb41affbf2dd86e3a6c38349e
Amin <34860@ma-web.nl>
2023-01-26 20:24:23 +0100

    bullit

--- js-game/game/index.js
+++ js-game/game/index.js
@@ -1,36 +1,45 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+
+const canvas = document.getElementById("canvas");
+const c = canvas.getContext("2d");
 
 canvas.width = innerWidth
 canvas.height = innerHeight
+
+function start()
+{  
+player.draw()
+animate();
+}
 
 class Player {
     constructor() {
         this.velocity = {
             x: 0,
             y: 0
-        }
+        };
 
         const image = new Image()
-        image.src = './img/Ship-Player.png'
+        image.src = 'img/Ship-Player.png'
         image.onload = () => {
-
-            this.image = image
-            this.width = image.width  
-            this.height = image.height 
+            const scale = 1.5;//0.15
+            this.image = image;
+            this.width = image.width * scale
+            this.height = image.height * scale
             this.position = {
-                x: canvas.width / 2 - this.width / 2,
-                y: canvas.height - this.height - 20
-            }    
-        }
-    }
+                x : canvas.width / 2 - this.width / 2,
+                y : canvas.height - this.height - 40
+            }
+            
+start()
 
+        } 
+    }
+            
     draw() {
-       // c.fillStyle = 'red'
-        //c.fillRect(this.position.x, this.position.y, this.width, this.height)
-       
+        //c.fillStyle = 'red'
+        //c.fillRect(this.position.x, this.position.y,this.width, this.height)
         c.drawImage(
-            this.image, 
+            this.image,
             this.position.x,
             this.position.y, 
             this.width, 
@@ -46,29 +55,111 @@ class Player {
     }
 }
 
+class Projectile {
+    constructor({ position, velocity}){
+        this.position = position
+        this.velocity = velocity
+
+        this.radius = 3
+    }
+
+    draw(){ 
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, 
+            Math.PI * 2)
+            c.fillStyle = 'red'
+            c.fill()
+            c.closePath()
+    }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
 const player = new Player()
-player.draw()
+const projectiles = []
+const keys ={
+    a: {
+        pressed: false
+    },
+    d: { 
+        pressed: false
+    },
+    k: { 
+        pressed: false
+    }
+    
+
+}
 
 function animate() {
     requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
+    projectiles.forEach((projectile, index) => {
+        if (projectile.position.y + projectile.radius <= 0) {
+            setTimeout(() => {
+                projectiles.splice(index, 1)
+            }, 0)
+        } else {
+            projectile.update()
+        }
+    })
+
+    if (keys.a.pressed && player.velocity.x >= 0)  {
+        player.velocity.x = -8
+    } else if (keys.d.pressed && player.velocity.x + player.width <= canvas.width) {
+        player.velocity.x = 5
+    } else{
+       player.velocity.x = 0 
+    }
+
 }
 
-animate()
 
-addEventListener('keydown', ({key}) => {
-    console.log('key')
+addEventListener('keydown', ({ key }) => {
     switch (key){
         case 'a':
-            console.log('left')
+            //console.log('left-down')
+            keys.a.pressed = true
             break
         case 'd':
-            console.log('right')
+            //console.log('right-down')
+            keys.d.pressed = true
             break
-        case ' ':
-            console.log('space')
+        case 'k':
+            //console.log('space-down')
+            projectiles.push(new Projectile({
+                position: {
+                    x: player.position.x + player.width / 2,
+                    y: player.position.y
+                },
+                velocity: {
+                    x: 0,
+                    y: -10
+                }
+            }))
+            //console.log(projectiles)
+            break
+    }
+})
+
+addEventListener('keyup', ({ key }) => {
+    switch (key){
+        case 'a':
+            //console.log('left-up')
+            keys.a.pressed = false
+            break
+        case 'd':
+            //console.log('right-up')
+            keys.d.pressed = false
+            break  
+        case 'k':
+            //console.log('space-up')
             break
 
     }
